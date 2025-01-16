e516e99acb1926a230b0a75c10a33a7d1113cccf
dhananjaykumare2003 <146513489+dhananjaykumare2003@users.noreply.github.com>
2025-01-16 10:47:04 +0530

    Update longest_consecutive_sequence.cpp

--- Array/longest_consecutive_sequence.cpp
+++ Array/longest_consecutive_sequence.cpp
@@ -1,4 +1,33 @@
-Approach :- basic logic but check all the constraints carefully (n == 0 && n == 1 && for loop condition from 0 to n-1 not upto n && condition when numbers are same)
+Approach 1:- Brute Force where for each element we will check can we get its consecutive greater numbers
+Time Complexity :- O(N^2)
+Space Complexity :- O(1)
+class Solution {
+public:
+    bool ls(int x,vector<int>& nums){
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] == x) return true;
+        }
+        return false;
+    }
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        int longest = 1,cnt = 1;
+        for(int i = 0; i < n; i++){
+            int x = nums[i];
+            int cnt = 1;
+            while(ls(x + 1,nums) == true){
+                x = x+1;
+                cnt++;
+            }
+            longest = max(longest,cnt);
+        }
+        return longest;
+    }
+};
+
+Approach- 2:- Better Approach, basic logic but check all the constraints carefully (n == 0 && n == 1 && for loop condition from 0 to n-1 not upto n && condition when numbers are same)
 Time Complexity :- O(NlogN) for sorting + O(N)
 Space Complexity :- O(1)
 
@@ -20,5 +49,33 @@ public:
             maxcnt = max(maxcnt,cnt);
         }
         return maxcnt;
+    }
+};
+
+Approach 3:- using unordered_set
+Time Complexity :- O(N)
+Space Complexity :- O(N)
+    
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        unordered_set<int> st;
+        for(int i = 0; i < n; i++){
+            st.insert(nums[i]);
+        }
+        int longest = 1;
+        for(auto it : st){
+            if(st.find(it-1) == st.end()){
+                int cnt = 1,x = it;
+                while(st.find(x+1) != st.end()){
+                    cnt++;
+                    x = x+1;
+                }
+                longest = max(longest,cnt);
+            }
+        }
+        return longest;
     }
 };
