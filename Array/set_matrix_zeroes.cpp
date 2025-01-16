0b15cc2d591915beffd7169f0ec519cbaf5bc374
dhananjaykumare2003 <146513489+dhananjaykumare2003@users.noreply.github.com>
2025-01-16 12:58:45 +0530

    Update set_matrix_zeroes.cpp

--- Array/set_matrix_zeroes.cpp
+++ Array/set_matrix_zeroes.cpp
@@ -28,3 +28,43 @@ public:
         }
     }
 };
+
+Approach :- storing rowindex and colindex in 0th row and col itself to reduce space but we need to handle the case of matrix[0][0]
+Time Complexity :- O(M*N)
+Space Complexity :- O(1)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        int col0 = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j != 0) matrix[0][j] = 0;
+                    else{
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
