5f28305bfb7cdcc98664fea9728579fb26ed4659
dhananjaykumare2003 <146513489+dhananjaykumare2003@users.noreply.github.com>
2025-02-01 10:37:27 +0530

    Create flatten_binary_tree_to_linked_list.cpp

---
+++ Binary Tree/flatten_binary_tree_to_linked_list.cpp
@@ -0,0 +1,21 @@
+Approach :- using logic
+Time Complexity :- O(N)
+Space Complexity :- O(1)
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr->left != NULL){
+                TreeNode* prev = curr->left;
+                while(prev->right){
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
+    }
+};
